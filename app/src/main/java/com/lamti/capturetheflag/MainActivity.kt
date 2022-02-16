57a873a61ab54f7d188f8205d6267d333f0769f3
TimLam92 <tilamprinos@gmail.com>
2022-02-17 00:08:54 +0200

    Integrate with compose

--- app/src/main/java/com/lamti/capturetheflag/MainActivity.kt
+++ app/src/main/java/com/lamti/capturetheflag/MainActivity.kt
@@ -2,6 +2,15 @@ package com.lamti.capturetheflag
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import com.lamti.capturetheflag.ui.CaptureTheFlagTheme
 
 class MainActivity : AppCompatActivity() {
 
@@ -9,5 +18,25 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        setContent {
+            CaptureTheFlagTheme {
+                ComposeScreen()
+            }
+        }
+    }
+
+}
+
+@Composable
+fun ComposeScreen() {
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(
+            text = "Compose Screen",
+            style = MaterialTheme.typography.body1,
+            color = MaterialTheme.colors.primary
+        )
     }
 }
