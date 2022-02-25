b9cc6d5bab6dba5fba2c87af01b5bcb26e4e3827
TimLam92 <tilamprinos@gmail.com>
2022-02-25 10:00:25 +0200

    Integrate google maps

--- app/src/main/java/com/lamti/capturetheflag/presentation/ui/fragments/maps/MapFragment.kt
+++ app/src/main/java/com/lamti/capturetheflag/presentation/ui/fragments/maps/MapFragment.kt
@@ -1,6 +1,56 @@
 package com.lamti.capturetheflag.presentation.ui.fragments.maps
 
+import android.os.Bundle
+import android.view.View
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.fragment.app.Fragment
+import com.google.android.gms.maps.GoogleMapOptions
+import com.google.android.gms.maps.model.CameraPosition
+import com.google.android.gms.maps.model.LatLng
+import com.google.maps.android.compose.GoogleMap
 import com.lamti.capturetheflag.R
+import com.lamti.capturetheflag.databinding.FragmentMapBinding
+import com.lamti.capturetheflag.presentation.ui.style.CaptureTheFlagTheme
 
-class MapFragment: Fragment(R.layout.fragment_map)
+class MapFragment : Fragment(R.layout.fragment_map) {
+
+    private var binding: FragmentMapBinding? = null
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = FragmentMapBinding.bind(view)
+
+        setupMapView()
+    }
+
+    private fun setupMapView() = binding?.run {
+        mapComposeView.setContent {
+            CaptureTheFlagTheme {
+                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
+                    MapView()
+                }
+            }
+        }
+    }
+
+    @Composable
+    fun MapView() {
+        val singapore = LatLng(37.93997336615248, 23.693031663615965)
+        GoogleMap(
+            modifier = Modifier.fillMaxSize(),
+            googleMapOptionsFactory = {
+                GoogleMapOptions().camera(CameraPosition.fromLatLngZoom(singapore, 24f))
+            }
+        )
+    }
+
+    override fun onDestroyView() {
+        binding = null
+        super.onDestroyView()
+    }
+
+}
